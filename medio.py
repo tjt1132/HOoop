2a721847bb9e7a9389e732a447db831977b4aa8e
Ticiano Torres Peralta <tjt1132@gmail.com>
2016-03-09 04:40:54 -0300

    - Implemente metodo reflejar() en clase Medio - Limitacion grande! Solo puede tomar un blanco por ahora.

--- medio.py
+++ medio.py
@@ -1,14 +1,19 @@
 class Medio(object):
 
     def __init__(self, blancos):
+        #List of blancos
         self.blancos = blancos
-
 
     def reflejar(self, una_senal, tiempo_inicial, tiempo_final):
         """
         Los blancos en el medio reflejan la senal
         """
-
         #TODO reflejar en un medio debe reflejar en todos los blancos de un medio
         #y devolver la senal reflejada
-        pass
+        #Done
+        #ret = list()
+
+        #for blanco in blancos:
+        #    ret.append(blanco.reflejar( senal, tiempo_inicial, tiempo_final ) ))
+
+        return self.blancos.reflejar( una_senal, tiempo_inicial, tiempo_final )
