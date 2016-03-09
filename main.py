492c5d89320e1c994b2be65bfc1c1e1febdfcaa5
Ticiano Torres Peralta <tjt1132@gmail.com>
2016-03-09 05:19:15 -0300

    - Implemented flow of main() - Crappy implementation, I'm tired.

--- main.py
+++ main.py
@@ -20,26 +20,43 @@ def main():
     import math
     # parametros del generador de senales
     amplitud = 0.2
-    fase = 1
-    frecuencia = 20*math.pi
+    fase = 0
+    frecuencia = math.pi / 8
 
     #TODO construir un nuevo genrador de senales
+    #Done
+    un_generador = generador.Generador(amplitud, fase, frecuencia)
 
     #TODO construir un detector
+    #Done
+    un_detector = detector.Detector()
 
     #TODO construir un nuevo radar
-
+    #Done
+    un_radar = radar.Radar(un_generador, un_detector)
 
     # parametros para un blanco
     amplitud_de_frecuencia_del_blanco = amplitud + 100
     tiempo_inicial_del_blanco = datetime.datetime(2016, 3, 5, 2)
     tiempo_final_del_blanco = datetime.datetime(2016, 3, 5, 4)
-    #TODO contruir un nuevo blanco
 
+    #TODO contruir un nuevo blanco
+    un_blanco = blanco.Blanco(amplitud_de_frecuencia_del_blanco, \
+    tiempo_inicial_del_blanco, tiempo_final_del_blanco )
 
     #TODO contruir un medio
+    un_medio = medio.Medio(un_blanco)
 
     #TODO construir un radar
+    un_radar = radar.Radar(un_generador, un_detector)
+
+    #detectar
+    detecto = un_radar.detectar(un_medio, tiempo_inicial, tiempo_final)
+
+    if detecto:
+        print "El radar detecto un blanco!"
+    else:
+        print "El rada no detecto nada!"
 
 if __name__ == "__main__":
     main()
