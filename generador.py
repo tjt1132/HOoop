60a69163a7224f28d7bf046cd41f581ddc5a0617
Ticiano Torres Peralta <tjt1132@gmail.com>
2016-03-09 00:54:55 -0300

    -Correji la generacion de la senal en generar() -Clase Generador completada

--- generador.py
+++ generador.py
@@ -6,25 +6,27 @@ Un generador de senal es el responsable de generar una senal portadora.
 class Generador(object):
 
     def __init__(self, amplitud, fase, frecuencia):
+        #Amplitud de la senal generada
         self.amplitud = amplitud
+        #Fase en porcentage de defasado
         self.fase = fase
+        #Frecuencia de la senal
         self.frecuencia = frecuencia
 
-        #  muestras por segundo
-        self.frecuencia_muestreo = frecuencia*3
-
+        #  Samples per second (Nyquist Rate)
+        self.frecuencia_muestreo = frecuencia*2
 
     def generar(self, tiempo_inicial, tiempo_final):
 
         import math
 
-        cantidad_muestras = (tiempo_final - tiempo_inicial).seconds/\
-        self.frecuencia_muestreo
+        cantidad_muestras = round((tiempo_final - tiempo_inicial).seconds * \
+        self.frecuencia_muestreo)
 
         muestras = range(cantidad_muestras)
         #TODO agregar un ruido blanco a la senal
 
-        ret = [self.amplitud*math.sin(2*(1/self.frecuencia)*i+self.fase) \
-        for i in muestras]
+        ret = [self.amplitud*math.sin( ( 8. / 3180. ) * self.frecuencia * i \
+        + ( math.pi * self.fase ) for i in muestras]
 
         return ret
