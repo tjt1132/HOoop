21c731d7f66ab3513e19c90cd47b65f24e750527
Ticiano Torres Peralta <tjt1132@gmail.com>
2016-03-09 04:58:32 -0300

    - Implemented a simple detectar() method for class Detector

--- detector.py
+++ detector.py
@@ -2,9 +2,16 @@ class Detector(object):
 
     def __init__(self):
         #TODO: completar con la inicializacion de los parametros del objeto
+        #Done
         pass
 
-    def detectar(self, senal):
+    def detectar(self, senal_original, senal):
 
         #TODO: Completar
-        pass
+        #Done
+        delta = [senal_original[i] - senal[i] for i in range(len(senal_original))]
+
+        if sum(delta):
+            return True
+        else:
+            return False
